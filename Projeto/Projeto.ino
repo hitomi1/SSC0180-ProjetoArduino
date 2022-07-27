b4bd31b5261bcb776b83acfce15c6c9df7e4f752
Gustavo Hitomi <gustavohitomi@gmail.com>
2022-07-27 14:06:28 -0300

    alterações no código

--- Projeto/Projeto.ino
+++ Projeto/Projeto.ino
@@ -1,25 +1,18 @@
-/*
-   Curso de Arduino e AVR 109
-   WR Kits Channel
-   Utilizando Sensor de Temperatura e Umidade DHT11
-   Autor: Eng. Wagner Rambo  Data: Março de 2017
-   www.wrkits.com.br | facebook.com/wrkits | youtube.com/user/canalwrkits | dailymotion.com/wrkits
-*/
 // ===============================================================================
 // --- Biblioteca Auxiliar ---
-#include <dht.h>   //biblioteca do sensor de umidade e temperatura
+#include  <dht.h>   //biblioteca do sensor de umidade e temperatura
 // ===============================================================================
 // --- Mapeamento de Hardware ---
-#define    dht_pin    5   //pino de sinal do dht11 ligado no digital 5
+#define   dht_pin    5   //pino de sinal do dht11 ligado no digital 5
 // ===============================================================================
 // --- Declaração de Objetos ---
-dht   my_dht;   //objeto para o sensor
+dht       my_dht;   //objeto para o sensor
 // ===============================================================================
 // --- Variáveis Globais ---
-int    temperatura = 0x00,   //armazena a temperatura em inteiro
-       umidade     = 0x00;   //armazena a umidade em inteiro
-#define ledBranco 9
-#define ledAmarelo 10
+int     temperatura = 0x00,   //armazena a temperatura em inteiro
+        umidade     = 0x00;   //armazena a umidade em inteiro
+#define ledBranco   9
+#define ledAmarelo  10
 // ===============================================================================
 // --- Configurações Iniciais ---
 void setup()
@@ -27,10 +20,7 @@ void setup()
   pinMode(ledBranco, OUTPUT);
   pinMode(ledAmarelo, OUTPUT);
   Serial.begin(9600);   //serial em 9600 baud rate
-
-} //end setup
-
-
+}
 // ===============================================================================
 // --- Loop Infinito ---
 void loop()
@@ -49,15 +39,15 @@ void loop()
   
   if(umidade>75){
     digitalWrite(ledAmarelo,HIGH);
-  }
+    }
     else{
       digitalWrite(ledAmarelo,LOW);
     }
-  
   
   Serial.print(temperatura);
   Serial.print("°C ");
   Serial.print(umidade);
   Serial.println("% ");
-  delay(750);
-} //end loop
+  delay(1000);
+  
+}
